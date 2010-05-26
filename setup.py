214486ebc9720edca54798273e42c41ca8dc2241
Jim Fulton <jim@zope.com>
2010-05-26 15:48:28 +0000

    Updated version.

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ default=zc.recipe.rhrc:uninstall
 name = 'zc.recipe.rhrc'
 setup(
     name = name,
-    version = '1.2dev',
+    version = '0',
     author = 'Jim Fulton',
     author_email = 'jim@zope.com',
     description = 'ZC Buildout recipe for Redhat RC scripts',
